39168f57e77ff498e2970f8d1d4cdfff3434addb
Vedant <admin@MngEnvMCAP797847.onmicrosoft.com>
2026-03-25 16:56:07 -0600

    Uncommented additional node pools

--- AKS-Terraform/Terraform-manifests/05-aks-linux-user-node-pool.tf
+++ AKS-Terraform/Terraform-manifests/05-aks-linux-user-node-pool.tf
@@ -1,32 +1,32 @@
-# # Dedicated Linux user node pool for Linux application workloads.
-# resource "azurerm_kubernetes_cluster_node_pool" "linux_user" {
-#   name                        = local.linux_user_node_pool_name
-#   kubernetes_cluster_id       = azurerm_kubernetes_cluster.aks.id
-#   vm_size                     = var.linux_user_node_pool_vm_size
-#   node_count                  = 1
-#   min_count                   = 1
-#   max_count                   = 3
-#   mode                        = "User"
-#   os_type                     = "Linux"
-#   os_disk_size_gb             = 30
-#   priority                    = "Regular"
-#   orchestrator_version        = var.kubernetes_version
-#   auto_scaling_enabled        = true
-#   zones                       = var.user_node_pool_zones
-#   vnet_subnet_id              = azurerm_subnet.linux_user_node_pool.id
-#   temporary_name_for_rotation = local.linux_user_node_pool_rotation_name
-#   node_labels                 = local.linux_user_node_pool_labels
-#   tags                        = local.linux_user_node_pool_tags
+# Dedicated Linux user node pool for Linux application workloads.
+resource "azurerm_kubernetes_cluster_node_pool" "linux_user" {
+  name                        = local.linux_user_node_pool_name
+  kubernetes_cluster_id       = azurerm_kubernetes_cluster.aks.id
+  vm_size                     = var.linux_user_node_pool_vm_size
+  node_count                  = 1
+  min_count                   = 1
+  max_count                   = 3
+  mode                        = "User"
+  os_type                     = "Linux"
+  os_disk_size_gb             = 30
+  priority                    = "Regular"
+  orchestrator_version        = var.kubernetes_version
+  auto_scaling_enabled        = true
+  zones                       = var.user_node_pool_zones
+  vnet_subnet_id              = azurerm_subnet.linux_user_node_pool.id
+  temporary_name_for_rotation = local.linux_user_node_pool_rotation_name
+  node_labels                 = local.linux_user_node_pool_labels
+  tags                        = local.linux_user_node_pool_tags
 
-#   upgrade_settings {
-#     max_surge                     = "10%"
-#     drain_timeout_in_minutes      = 0
-#     node_soak_duration_in_minutes = 0
-#   }
+  upgrade_settings {
+    max_surge                     = "10%"
+    drain_timeout_in_minutes      = 0
+    node_soak_duration_in_minutes = 0
+  }
 
-#   lifecycle {
-#     ignore_changes = [
-#       node_count,
-#     ]
-#   }
-# }
+  lifecycle {
+    ignore_changes = [
+      node_count,
+    ]
+  }
+}
--- AKS-Terraform/Terraform-manifests/06-aks-windows-user-node-pool.tf
+++ AKS-Terraform/Terraform-manifests/06-aks-windows-user-node-pool.tf
@@ -1,33 +1,33 @@
-# # Dedicated Windows user node pool for Windows application workloads.
-# resource "azurerm_kubernetes_cluster_node_pool" "windows_user" {
-#   name                        = local.windows_user_node_pool_name
-#   kubernetes_cluster_id       = azurerm_kubernetes_cluster.aks.id
-#   vm_size                     = var.windows_user_node_pool_vm_size
-#   node_count                  = 1
-#   min_count                   = 1
-#   max_count                   = 3
-#   mode                        = "User"
-#   os_type                     = "Windows"
-#   os_sku                      = "Windows2022"
-#   os_disk_size_gb             = 50
-#   priority                    = "Regular"
-#   orchestrator_version        = var.kubernetes_version
-#   auto_scaling_enabled        = true
-#   zones                       = var.user_node_pool_zones
-#   vnet_subnet_id              = azurerm_subnet.windows_user_node_pool.id
-#   temporary_name_for_rotation = local.windows_user_node_pool_rotation_name
-#   node_labels                 = local.windows_user_node_pool_labels
-#   tags                        = local.windows_user_node_pool_tags
+# Dedicated Windows user node pool for Windows application workloads.
+resource "azurerm_kubernetes_cluster_node_pool" "windows_user" {
+  name                        = local.windows_user_node_pool_name
+  kubernetes_cluster_id       = azurerm_kubernetes_cluster.aks.id
+  vm_size                     = var.windows_user_node_pool_vm_size
+  node_count                  = 1
+  min_count                   = 1
+  max_count                   = 3
+  mode                        = "User"
+  os_type                     = "Windows"
+  os_sku                      = "Windows2022"
+  os_disk_size_gb             = 50
+  priority                    = "Regular"
+  orchestrator_version        = var.kubernetes_version
+  auto_scaling_enabled        = true
+  zones                       = var.user_node_pool_zones
+  vnet_subnet_id              = azurerm_subnet.windows_user_node_pool.id
+  temporary_name_for_rotation = local.windows_user_node_pool_rotation_name
+  node_labels                 = local.windows_user_node_pool_labels
+  tags                        = local.windows_user_node_pool_tags
 
-#   upgrade_settings {
-#     max_surge                     = "10%"
-#     drain_timeout_in_minutes      = 0
-#     node_soak_duration_in_minutes = 0
-#   }
+  upgrade_settings {
+    max_surge                     = "10%"
+    drain_timeout_in_minutes      = 0
+    node_soak_duration_in_minutes = 0
+  }
 
-#   lifecycle {
-#     ignore_changes = [
-#       node_count,
-#     ]
-#   }
-# }
+  lifecycle {
+    ignore_changes = [
+      node_count,
+    ]
+  }
+}
